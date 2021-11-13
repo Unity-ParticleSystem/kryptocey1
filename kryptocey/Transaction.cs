cc9a0af912cdcb1bc433dc93e6f364628e7b500f
Unity-ParticleSystem <74088006+Unity-ParticleSystem@users.noreply.github.com>
2021-11-13 15:02:08 -0500

    Add files via upload

---
+++ kryptocey/Transaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Utils;
+
+
+namespace Models
+{
+
+    public class Transaction
+    {
+        
+        public long TimeStamp { get; set; }
+        public string Sender { set; get; }
+        public string Recipient { set; get; }
+        public double Amount { set; get; }
+        public double Fee { set; get; }
+    }
+
+}
